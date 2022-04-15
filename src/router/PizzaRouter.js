1d4efc7de41b5841cc47bf4a2226f8c0b70d679a
Mateus Augusto <85250525+Mateus-AAC@users.noreply.github.com>
2022-04-15 10:41:36 -0300

    Update PizzaRouter.js

--- src/router/PizzaRouter.js
+++ src/router/PizzaRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const PizzasController = require('../controllers/PizzasController');3
+const PizzasController = require('../controllers/PizzasController');
 
 router.get('/', PizzasController.listar);
 
